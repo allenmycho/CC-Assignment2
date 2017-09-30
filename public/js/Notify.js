59b99f636a27a7d7d47a87e9c35000a65b800d8f
alexfonseka <s3507870@student.rmit.edu.au>
2017-09-30 18:21:45 +1000

    System now reacts to Rain/Thunderstorm weather and notifies the user.

--- public/js/Notify.js
+++ public/js/Notify.js
@@ -1,6 +1,8 @@
 var notifications = [];
 notifications[0] = "<h3>Heads up!</h3><br>";
 
+var forecastArray = [];
+
 function getEvents(Events) {
 
     for (i = 0; i < Events.length; i++) {
@@ -10,16 +12,55 @@ function getEvents(Events) {
         var res = rawDate.split("T");
         var eventDate = res[0];
 
+
         var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var d = new Date(res[0]);
         var eventDayName = days[d.getDay()];
 
-        notify(eventName + " on " + eventDayName, "Unknown weather");
+        var date = eventDate.split("-");
+
+        var weather = anologize(eventDate);
+
+        if (weather === "Rain" || weather === "Thunderstorm") {
+            notify(eventName + " on " + eventDayName + " " + date[2], weather);
+        }
 
     }
 
     displayNotifications();
 
+}
+
+function anologize(eventDate) {
+
+    var applicable = false;
+    var condition = null;
+
+    for (var i = 0; i < forecastArray.length; i++) {
+
+        if (eventDate === forecastArray[i][1]) {
+            applicable = true;
+            condition = forecastArray[i][0];
+
+        }
+    }
+
+    if (condition === "Rain") {
+        return condition;
+    }
+    if (condition === "Thunderstorm") {
+        return condition;
+    }
+    if (condition === null) {
+        return null;
+    }
+
+}
+
+function saveForecast(forecast) {
+
+    forecastArray = forecast;
+    console.log(forecastArray);
 }
 
 
--- public/js/weather.js
+++ public/js/weather.js
@@ -1,5 +1,4 @@
 function setup() {
-    var weather;
     var api = 'http://api.openweathermap.org/data/2.5/weather?';
     var city = 'q=Melbourne,AU&';
     var appID = 'APPID=8c9f87b1916ca231f89f3254eb00c5c4&';
@@ -7,18 +6,6 @@ function setup() {
 
     var url = api + city + appID + units;
     loadJSON(url, printWeather);
-    console.log(url);
-
-
-    var t = setTimeout(setup, 600000);
-}
-
-function retrieveEvents(forecast) {
-    console.log("forecast: " + forecast);
-    // weatherForecast(forecast);
-}
-
-function weatherForecast(events) {
 
     var forecast_api = 'http://api.openweathermap.org/data/2.5/forecast?';
     var forecast_city = 'q=Melbourne,AU&';
@@ -26,9 +13,42 @@ function weatherForecast(events) {
     var forecast_units = 'units=metric';
 
     var forecast_url = forecast_api + forecast_city + forecast_appID + forecast_units;
+    loadJSON(forecast_url, processForecast);
 
-    loadJSON(forecast_url, retrieveEvents);
+    var t = setTimeout(setup, 600000);
+}
 
+function processForecast(forecast) {
+
+    var res = (forecast.list[0].dt_txt).split("-");
+    var day = res[2].split(" ");
+    var prevDate = day[0];
+
+    var forecastArray = [];
+
+    for (var i = 0; i < forecast.list.length; i++) {
+        var res = (forecast.list[i].dt_txt).split("-");
+        var day = res[2].split(" ");
+
+        if (day[0] === prevDate) {
+            var date = (forecast.list[i].dt_txt).split(" ");
+            forecastArray.push([(forecast.list[i].weather[0].main), date[0]]);
+            break;
+        }
+    }
+    for (var i = 0; i < forecast.list.length; i++) {
+
+        var res = (forecast.list[i].dt_txt).split("-");
+        var day = res[2].split(" ");
+
+        if (day[0] !== prevDate) {
+            prevDate = day[0];
+            var date = (forecast.list[i].dt_txt).split(" ");
+            forecastArray.push([(forecast.list[i].weather[0].main), date[0]]);
+        }
+    }
+
+    saveForecast(forecastArray);
 }
 
 function printWeather(weather) {
@@ -46,5 +66,6 @@ function printWeather(weather) {
     document.getElementById('weatherNow').innerHTML = tempRounded + '&deg' + 'C ' + main;
     document.getElementById('weatherHigh').innerHTML = 'Feels like ' + high + '&deg' + 'C ';
     document.getElementById('weatherIcon').src = "images/icons/" + iconId + ".png";
+
 
 }
