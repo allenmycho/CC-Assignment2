1e4c5181c77abe757313e4aa01cf6f1ed2894a22
alexfonseka <s3507870@student.rmit.edu.au>
2017-09-30 19:14:34 +1000

    Minor changes to day/night background system.

--- public/js/Helper.js
+++ public/js/Helper.js
@@ -1,5 +1,5 @@
 var currentTime = new Date().getHours();
-if (7 <= currentTime && currentTime < 20) {
+if (7 <= currentTime && currentTime < 19) {
     if (document.body) {
         document.body.background = "images/backgrounds/Background_Day.png";
     }
